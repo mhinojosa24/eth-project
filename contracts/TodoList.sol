766e06c5bba35f91ea8cbc2872382c4db8e3f7e2
Maximo Hinojosa <maximohinojosa13@gmail.com>
2020-10-05 18:13:26 -0700

    [add] project setup

---
+++ contracts/TodoList.sol
@@ -0,0 +1,6 @@
+pragma solidity >=0.5.16;
+
+contract TodoList {
+    // state variables
+    uint public taskCount = 0; // NOTE: use public key word to read this state variable; soludity gives a fucntion that allows us to read
+}
