265263cfe28f85c22d2dd4d9eebecf1bd2f0ea16
Maximo Hinojosa <maximohinojosa13@gmail.com>
2020-10-12 00:43:14 -0400

    [add] user can create task

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,27 +1,36 @@
 pragma solidity >=0.5.16;
 
 contract TodoList {
-    // state variables
-    uint public taskCount = 0; // NOTE: use public key word to read this state variable; soludity gives a fucntion that allows us to read
+  // state variables
+  uint public taskCount = 0; // NOTE: use public key word to read this state variable; soludity gives a fucntion that allows us to read
 
-    // model for task
-    struct Task {
-        uint id; // unsigned integer
-        string content;
-        bool completed; // state of the checked box
-    }
+  // model for task
+  struct Task {
+      uint id; // unsigned integer
+      string content;
+      bool completed; // state of the checked box
+  }
 
-    // access storage
-    mapping(uint => Task) public tasks; // storing key-value pair; id of the task
+  // access storage
+  mapping(uint => Task) public tasks; // storing key-value pair; id of the task
 
-    constructor() public {
-        createTask("Check out maximo's new task");
-    }
+  // create event
+  event TaskCreated(
+      uint id,
+      string content,
+      bool completed
+  );
 
-    // create task
-    function createTask(string memory _content) public {
-        taskCount ++; // update task count everytime a new task is created
-        tasks[taskCount] = Task(taskCount, _content, false);
-    }
+  constructor() public {
+      createTask("Check out maximo's new task");
+  }
 
+  // create task
+  function createTask(string memory _content) public {
+      taskCount ++; // update task count everytime a new task is created
+      tasks[taskCount] = Task(taskCount, _content, false);
+      emit TaskCreated(taskCount, _content, false);
+  }
 }
+
+  
--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ App = {
     contracts: {},
 
     load: async () => {
-        // Load app..
         await App.loadWeb3() // load the web view to connect to the blockchain
         await App.loadAccount()
         await App.loadContract()
@@ -12,23 +11,32 @@ App = {
     },
 
     // NOTE: use metamask library to talk to the ethereum blockchain
-    // https://medium.com/metamask/https-medium-com-metamask-breaking-change-injecting-web3-7722797916a8
     loadWeb3: async () => {
-        if (typeof web3 !== 'undefined') {
+
+        let web3Injected = window.web3;
+        if (typeof web3Injected !== 'undefined') {
+            // detect MetaMask.io
             App.web3Provider = web3.currentProvider
-            web3 = new Web3(web3.currentProvider)
+            web3 = new Web3(web3Injected.currentProvider)
+            ethereum.autoRefreshOnNetworkChange = false
         } else {
-            web3Provider = new Web3.providers.HttpProvider('http://127.0.0.1:7545');
+            // eth node
+            web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:3000'))
         }
+        // if (typeof web3 !== 'undefined') {
+        //     App.web3Provider = web3.currentProvider
+        //     web3 = new Web3(web3.currentProvider)
+        // } else {
+        //     App.web3Provider = new Web3.providers.HttpProvider('HTTP://127.0.0.1:7545');
+        // }
         
         if (window.ethereum) {
-            web3 = new Web3((window.ethereum));
             window.web3 = new Web3(ethereum);
-            try {
-                await ethereum.enable();
-            } catch (error) {
-                console.error(error);
-            }
+            window.ethereum.enable().then(() => {
+                // web3.eth.sendTransaction({ from: App.account })
+            }, reason => {
+                console.log('reason: ', reason)
+            })
         }
     },
 
@@ -40,7 +48,7 @@ App = {
 
     loadContract: async () => {
         // NOTE: creating a JavaScript version of the smart contract
-        const todoList = await $.getJSON('TodoList.json')
+        const todoList = await $.getJSON('TodoList.json') // get json
         App.contracts.TodoList = TruffleContract(todoList) // create wrapper around the contract by truffle
         App.contracts.TodoList.setProvider(App.web3Provider)
 
@@ -65,20 +73,6 @@ App = {
         App.setLoading(false)
     },
 
-    setLoading: (boolean) => {
-        App.loading = boolean
-        const loader = $('#loader')
-        const content = $('#content')
-
-        if (boolean) {
-            loader.show()
-            content.hide()
-        } else {
-            loader.hide()
-            content.show()
-        }
-    },
-
     renderTasks: async () => {
         // load the tasks form the blockchain
         const taskCount = await App.todoList.taskCount()
@@ -96,9 +90,9 @@ App = {
             const $newTaskTemplate = $taskTemplate.clone()
             $newTaskTemplate.find('.content').html(taskContent)
             $newTaskTemplate.find('input')
-                            .prop('name', taskId)
-                            .prop('checked', taskCompleted)
-                            // .on('click', App.toggleCompleted)
+                .prop('name', taskId)
+                .prop('checked', taskCompleted)
+                .on('click', App.toggleCompleted)
 
             if (taskCompleted) {
                 $('#completedTaskList').append($newTaskTemplate)
@@ -108,6 +102,29 @@ App = {
             // Show the task
             $newTaskTemplate.show()
         }
+    },
+
+    createTask: async () => {
+        App.setLoading(true)
+        const content = $('#newTask').val() // get value from form input
+        console.log(content)
+        const result = await App.todoList.createTask(content) // error: Uncaught (in promise) Error: The send transactions "from" field must be defined!
+        console.log("result: ", result)
+        window.location.reload() // refresh the page to get all tasks from blockchain
+    },
+
+    setLoading: (boolean) => {
+        App.loading = boolean
+        const loader = $('#loader')
+        const content = $('#content')
+
+        if (boolean) {
+            loader.show()
+            content.hide()
+        } else {
+            loader.hide()
+            content.show()
+        }
     }
 }
 
@@ -116,3 +133,4 @@ $(() => {
         App.load()
     })
 })
+
